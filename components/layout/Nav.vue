01897c661fe7aed8158d42445902ce2917e371a9
brotherly <chhunly.smart@gmail.com>
2024-07-28 18:45:58 +0700

    navigation icon

--- components/layout/Nav.vue
+++ components/layout/Nav.vue
@@ -1,40 +1,42 @@
 <script setup lang="ts">
+import {useIcon} from "~/composables/useIcon";
+
 const route = useRoute()
 
 const links = [
     {
         label: 'Home',
-        icon: 'i-heroicons-home',
+        icon: useIcon().home,
         to: '/'
     },
     {
         label: 'Orders',
-        icon: 'i-heroicons-home',
+        icon: useIcon().list,
         to: '/orders'
     },
     {
         label: 'invoices',
-        icon: 'i-heroicons-home',
+        icon: useIcon().invoice,
         to: '/invoices'
     },
     {
         label: 'customers',
-        icon: 'i-heroicons-home',
+        icon: useIcon().customer,
         to: '/participants/customers'
     },
     {
         label: 'staffs',
-        icon: 'i-heroicons-home',
+        icon: useIcon().staff,
         to: '/participants/staffs'
     },
     {
         label: 'materials',
-        icon: 'i-heroicons-home',
+        icon: useIcon().material,
         to: '/materials'
     },
     {
         label: 'config',
-        icon: 'i-heroicons-home',
+        icon: useIcon().setting,
         to: '/config'
     },
 ]
@@ -49,7 +51,7 @@ const links = [
             active: 'text-gray-900 dark:text-white before:bg-primary-400 dark:before:bg-primary-600/50',
             inactive: 'dark:text-gray-400 hover:text-primary-600 hover:bg-primary dark:hover:text-white hover:before:bg-primary-300 dark:hover:before:bg-gray-800/75',
             icon: {
-                inactive: 'dark:text-primary-800 group-hover:text-gray-700 dark:group-hover:text-gray-200',
+                inactive: 'dark:text-primary-600 text-primary-600 group-hover:text-gray-700 dark:group-hover:text-gray-200',
             },
             badge: {
                 base: 'absolute right-0 rounded',
